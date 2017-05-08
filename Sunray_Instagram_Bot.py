7e5676e2ef557195b62cb84895f1160731c1f5ff
jockonarock <jockonarock@users.noreply.github.com>
2017-05-08 16:31:59 +0800

    Multiple functionality updates
    Reduced number of API calls
    Corrected private profile management
    Reset throttle timings

--- Sunray_Instagram_Bot.py
+++ Sunray_Instagram_Bot.py
@@ -5,18 +5,21 @@ from pprint import pprint
 import json
 import datetime
 import time
-from random import randint
+import random
+from random import randint, shuffle
 
 timemark = datetime.datetime.now()
+print ("Sunray IG Bot started at " + str(timemark))
 
+my_username = "" #your username
+my_password = "" #your password
 
-
-API = InstagramAPI("login","password") #your username and password
-API.login()
-
-userid = 0 #add the userid of your account
+userid = 0 #your userid
 mediaid = 0 
-maxid = ''
+
+API = InstagramAPI(my_username, my_password) #your username and password
+print ("Logging into %s" % my_username)
+API.login()
 
 accounts= {
 
@@ -31,7 +34,7 @@ accounts= {
 		
 		'key3':284341891,
 		'key4':22963539,
-		'key5' :259394152
+		'key5':259394152
 	}
 }
 
@@ -39,8 +42,9 @@ accounts= {
 #Speed control to prevent ban
 
 def throttle():
-	sleeptime = randint(5,10)
-	print ("Throttle in effect: bot sleeping for %i seconds.\n" %sleeptime)
+	sleeptime = randint(25,35)
+	print ("Throttle in effect: bot sleeping for %i seconds." %sleeptime)
+	print ("Paused at %s\n" %str(datetime.datetime.now()))
 	time.sleep(sleeptime)
 
 
@@ -55,17 +59,15 @@ def follow_procedure():
 	#Generate a recent follower pool for universities instagram accounts
 	for sets in accounts:
 		for key,value in accounts[sets].items():
-			#print (str(value))
 
 			API.getUserFollowers(value)
 			for i in API.LastJson['users']:
 				follow_pool.append(i['pk'])
-			time.sleep(1)
-
-	#print (follow_pool)
+			
 	print ("Follow Pool - Harvested %i\n" %len(follow_pool))
 
 	#Follow harvested profiles in the follow_pool list
+	random.shuffle(follow_pool)
 	for i in follow_pool:
 		legit_user = True
 		API.userFriendship(i)
@@ -73,8 +75,10 @@ def follow_procedure():
 
 		API.getUsernameInfo(i)
 		i_username = API.LastJson['user']['username']
-
-		print ("---Retriving user data for: %s ---" %i_username)
+		i_follower = API.LastJson['user']['follower_count']
+		i_following = API.LastJson['user']['following_count']
+		
+		print ("---Retriving user data for: %s---" %i_username)
 		
 		#Skip if already following/request sent
 		if friendship_status['outgoing_request'] == True or friendship_status['following'] == True:
@@ -83,12 +87,7 @@ def follow_procedure():
 			continue
 
 		else:
-
 			try:
-
-				i_follower = len(API.getTotalFollowers(i))
-				i_following = len(API.getTotalFollowings(i))
-
 				#Check for Celebgram
 				if i_follower/i_following >=2:
 					legit_user = False
@@ -113,15 +112,9 @@ def follow_procedure():
 					follow_pool.remove(i)
 					throttle()
 
-			#Follow private profile
 			except:
-				print ("Private Account, attempting to follow")
-				API.follow(i)
-				follow_counter += 1
-				print ("Followed %s, Session Followed #%i\n" %(i_username, follow_counter))
-				follow_pool.remove(i)
-				throttle()
-
+				print ("Error.\n")
+				continue
 
 
 
